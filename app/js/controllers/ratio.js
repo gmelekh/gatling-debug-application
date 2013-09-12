0d2a68d83dc5efc0140750afd16769a0e6e7d611
Guillaume Corré <gui.corre@gmail.com>
2013-09-12 17:57:53 +0200

    Compute ratio

--- app/js/controllers/ratio.js
+++ app/js/controllers/ratio.js
@@ -1,7 +1,12 @@
-/* global gatling */
+/* global gatling, debugData */
 
 'use strict';
 
 gatling.controller('ratioController', function ($rootScope) {
-  $rootScope.ratio = 0.5;
+  var start = debugData[0].requestStartDate,
+    end = debugData[debugData.length - 1].responseStartDate;
+
+  // TODO magic value: timeline-col-14 width without margin,
+  // plus supposed following span length
+  $rootScope.ratio = 549 / (end - start + 100);
 });
